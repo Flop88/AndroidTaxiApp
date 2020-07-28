c93032e4980b5662d75748458d30af9ab6a50c99
Maksim Likhachev <wwwqwwwqwwwqwwwq@gmail.com>
2020-07-28 10:25:31 +0300

    implement chooseModeActivity

--- app/src/main/java/ru/mvlikhachev/texiapp/Activities/ChooseModeActivity.java
+++ app/src/main/java/ru/mvlikhachev/texiapp/Activities/ChooseModeActivity.java
@@ -2,7 +2,9 @@ package ru.mvlikhachev.texiapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import ru.mvlikhachev.texiapp.R;
 
@@ -12,5 +14,19 @@ public class ChooseModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                PassengerSignInActivity.class
+        ));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                DriverSignInActivity.class
+        ));
     }
 }
---
+++ app/src/main/java/ru/mvlikhachev/texiapp/Activities/DriverSignInActivity.java
@@ -0,0 +1,16 @@
+package ru.mvlikhachev.texiapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.mvlikhachev.texiapp.R;
+
+public class DriverSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_in);
+    }
+}
